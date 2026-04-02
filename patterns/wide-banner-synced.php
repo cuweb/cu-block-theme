7ccbcbfd55f492a135508de923d9b09ec72d2fd4
Bhoomi Pipalia <bhoomipipalia@Bhoomis-MacBook-Pro-2.local>
2026-04-02 15:08:05 -0400

    add: wide banner pattern

---
+++ patterns/wide-banner-synced.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Wide Banner Synced
+ * Slug: carleton-block-theme/wide-banner-synced
+ * Categories: 
+ */
+?>
+<!-- wp:cover {"url":"http://localhost:8882/wp-content/uploads/2026/03/P_CarletonPhotography_Aerial_170122-56-1600x700-1.jpg","dimRatio":70,"isUserOverlayColor":true,"tagName":"section","sizeSlug":"full","metadata":{"name":"Wide Banner"},"align":"wide","className":"wide-banner","style":{"border":{"radius":{"topLeft":"12px","topRight":"12px","bottomLeft":"12px","bottomRight":"12px"}},"elements":{"heading":{"color":{"background":"var:preset|color|black"}}}},"layout":{"type":"constrained"}} -->
+<section class="wp-block-cover alignwide wide-banner" style="border-top-left-radius:12px;border-top-right-radius:12px;border-bottom-left-radius:12px;border-bottom-right-radius:12px"><img class="wp-block-cover__image-background  size-full" alt="" src="http://localhost:8882/wp-content/uploads/2026/03/P_CarletonPhotography_Aerial_170122-56-1600x700-1.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-70 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"metadata":{"name":"Content"},"className":"wide-banner__content","layout":{"type":"constrained","contentSize":"70%","justifyContent":"center","wideSize":"0%"}} -->
+<div class="wp-block-group wide-banner__content"><!-- wp:group {"style":{"spacing":{"padding":{"top":"24px","bottom":"24px","left":"32px","right":"32px"},"blockGap":"var:preset|spacing|40"},"border":{"radius":{"topLeft":"12px","topRight":"12px","bottomLeft":"12px","bottomRight":"12px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-top-left-radius:12px;border-top-right-radius:12px;border-bottom-left-radius:12px;border-bottom-right-radius:12px;padding-top:24px;padding-right:32px;padding-bottom:24px;padding-left:32px"><!-- wp:heading -->
+<h2 class="wp-block-heading">Test</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Add some paragraph text, nec tempus nibh scelerisque. Aliquam a porttitor massa. Nunc molestie commodo tortor non sagittis.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">View Program Site</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-secondary"} -->
+<div class="wp-block-button is-style-secondary"><a class="wp-block-button__link wp-element-button">Apply Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></section>
+<!-- /wp:cover -->
---
+++ patterns/wide-banner.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Wide Banner
+ * Slug: carleton-block-theme/wide-banner
+ * Categories: 
+ */
+?>
+<!-- wp:cover {"url":"http://localhost:8882/wp-content/uploads/2026/03/P_CarletonPhotography_Aerial_170122-56-1600x700-1.jpg","dimRatio":70,"isUserOverlayColor":true,"tagName":"section","sizeSlug":"full","metadata":{"name":"Wide Banner"},"align":"wide","className":"wide-banner","style":{"border":{"radius":{"topLeft":"12px","topRight":"12px","bottomLeft":"12px","bottomRight":"12px"}},"elements":{"heading":{"color":{"background":"var:preset|color|black"}}}},"layout":{"type":"constrained"}} -->
+<section class="wp-block-cover alignwide wide-banner" style="border-top-left-radius:12px;border-top-right-radius:12px;border-bottom-left-radius:12px;border-bottom-right-radius:12px"><img class="wp-block-cover__image-background  size-full" alt="" src="http://localhost:8882/wp-content/uploads/2026/03/P_CarletonPhotography_Aerial_170122-56-1600x700-1.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-70 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"metadata":{"name":"Content"},"className":"wide-banner__content","layout":{"type":"constrained","contentSize":"50%","justifyContent":"center"}} -->
+<div class="wp-block-group wide-banner__content"><!-- wp:group {"style":{"spacing":{"padding":{"top":"24px","bottom":"24px","left":"32px","right":"32px"},"blockGap":"var:preset|spacing|40"},"border":{"radius":{"topLeft":"12px","topRight":"12px","bottomLeft":"12px","bottomRight":"12px"}}},"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-black-background-color has-background" style="border-top-left-radius:12px;border-top-right-radius:12px;border-bottom-left-radius:12px;border-bottom-right-radius:12px;padding-top:24px;padding-right:32px;padding-bottom:24px;padding-left:32px"><!-- wp:heading -->
+<h2 class="wp-block-heading">Test</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">View Program Site</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-secondary"} -->
+<div class="wp-block-button is-style-secondary"><a class="wp-block-button__link wp-element-button">Apply Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></section>
+<!-- /wp:cover -->
